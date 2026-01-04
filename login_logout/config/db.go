8b0c67ec65741c95dec785c6bc5aba13f1bcd989
Alisha <alisha.official.xvi@gmail.com>
2026-01-04 12:18:16 +0400

    Add config folder and Go modules

---
+++ login_logout/config/db.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func ConnectDB() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Test connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Cannot connect to MongoDB:", err)
+	}
+
+	Client = client
+	log.Println("Connected to MongoDB!")
+}
