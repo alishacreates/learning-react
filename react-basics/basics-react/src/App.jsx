b81e94fb437e4327415e2631b30ba5c639760f8e
Alisha <alishaofficial.xvi@gmail.com>
2026-01-07 00:06:32 +0400

    pizza

--- react-basics/basics-react/src/App.jsx
+++ react-basics/basics-react/src/App.jsx
@@ -5,7 +5,9 @@ import './App.css';
 const App = () => {
   return (
     <div className='main-container'>
-      <Pizza name= "Margherita" price="200"/>
+      <Pizza number = {1} name= "Margherita" price="200"/>
+      <Pizza number = {2} name= "Paneer Tikka" price="300"/>
+      <Pizza number = {3} name= "Farmhouse" price="250"/>
     </div>
   )
 }
--- react-basics/basics-react/src/components/Pizza.jsx
+++ react-basics/basics-react/src/components/Pizza.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useActionState, useState } from 'react'
+import './Pizza.css'
 
-const Pizza = (props) => {
+const Pizza = ({number, name, price}) => {
+
+  const [quantity, setQuantity] = useState(1);
+  
+  increaseQuantity(() => {
+    setQuantity => (quantity => quantity +1)
+  })
+
+   decreaseQuantity(() => {
+    setQuantity => (quantity => quantity -1)
+  })
+  
   return (
-    <div>
-        <h2> Pizza </h2>
-        <p>Pizza name: {props.name}</p>
-        <p>Pizza price: {props.price}</p>
+    <div className='container'>
+            <h2> Pizza {number} </h2>
+            <p>Pizza name: {name}</p>
+            <p>Pizza price: ${price}</p>
+            <h4 className='quantity'>Quantity {quantity}</h4>
+            <h5><button onClick={increaseQuantity()}>+</button>  Increase </h5>
+            <h5><button onClick={decreaseQuantity()}>-</button>  Decrease </h5>
     </div>
   )
 }
