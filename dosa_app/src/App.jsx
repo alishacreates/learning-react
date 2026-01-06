83a52116d5c7cacd926dc5038558e7d3c2fd755f
Alisha <alishaofficial.xvi@gmail.com>
2026-01-07 00:54:27 +0400

    done

--- dosa_app/src/App.jsx
+++ dosa_app/src/App.jsx
@@ -5,11 +5,15 @@ import './App.css';
 
 const App = () => {
   return (
+  <>
+  <h1 className='heading'>Dosa Buying Website</h1>
     <div className='main-container'>
       <Dosa number = {1} name= "Paneer Dosa" price={200}/>
       <Dosa number = {2} name= "Masala Dosa" price={300}/>
       <Dosa number = {3} name= "Cheese Dosa" price={250}/>
+      <Dosa number = {4} name= "Rava Dosa" price={230}/>
     </div>
+  </>
   )
 }
 
