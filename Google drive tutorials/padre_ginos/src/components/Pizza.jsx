9d03540a6300d24f4195b21bb216396fbc894689
Alisha <alishaofficial.xvi@gmail.com>
2026-01-12 14:23:15 +0400

    converting alljs to jsx

---
+++ Google drive tutorials/padre_ginos/src/components/Pizza.jsx
@@ -0,0 +1,7 @@
+// const Pizza = (props) => {
+//     return React.createElement("div", {}, [
+//         React.createElement("h1", {}, props.name),
+//         React.createElement("p", {}, props.description),
+//     ]);
+// };
+
